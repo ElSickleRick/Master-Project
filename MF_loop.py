6f9f227323d5eb29052d83db6d83d77613cc7001
Eric Kürschner <erickuerschner@gmx.de>
2026-05-08 11:14:29 +0200

    eod 080526

--- MF_loop.py
+++ MF_loop.py
@@ -4,31 +4,28 @@ from multiprocessing import pool
 
 class MF_loop:
 
-    def __init__(self, beta, N_diff_bound, mu_step, Chi_diff_bound, Chi_step):
-        # possible pass on parameters: beta, N_diff_bound, mu_step, Chi_diff_bound, Chi_step
-        # maybe pass on eival and eivec as well?
+
+    def __init__(self, pop_link_dict,  beta, eival, eivec,  N_diff_bounid, mu_step,  Chi_abs_dif_bound, Chi_ph_dif_bd,  Chi_step):
+	# mu step probably has to be adjusetd!
+	
+	self.pop_link_dict = pop_link_dict
         self.beta = beta 
-        self.N_diff_bound = N_diff_bound 
-        self.mu_step = mu_step
-        self.Chi_diff_bound = Chi_diff_bound
+	self.eival = eival
+	self.eivec = eivec 
+        self.N_dif_bound = N_dif_bound
+	self.mu_step = mu_step
+        self.Chi_abs_dif_bound = Chi_abs_dif_bound
+	self.Chi_ph_dif_bd = Chi_ph_dif_bd
         self.Chi_step = Chi_step
-        return 
+        return
 
-    def Ham_diag(Ham): #merge with thermal_calc?
 
-        eival, eivec = la.eigh(Ham, lower=False)
-
-        return eival, eivec  
-
-    def thermal_calc(self, eival):
+    def thermal_calc(self):
         '''
         Calculates the Boltzman weights as well as the partition fucntion
 
         Parameters:
         ----------
-        eival: array of floats 
-                eigenvalues of Hamiltonains, ascendingly ordered
-
         Returns:
         -------
         weights: array of floats
@@ -36,11 +33,12 @@ class MF_loop:
         Z: float
                 partition function
         '''
-        weights = np.exp(-self.beta*eival)
+        weights = np.exp(-self.beta*self.eival)
 
         return weights, np.sum(weights)
     
-    def N_calc(Z, eivec, weights):
+
+    def N_calc(self, Z, weights):
 
         '''
         Calculates the expectation value of N on all sites 
@@ -49,8 +47,7 @@ class MF_loop:
         ---------
         Z: float
                 partition function
-        eivec: 2D array of floats
-                matrix of eigenvectors of Hamiltonian (eigenvectors are columns)
+
         weights: array of floats 
                 thermal (Boltzman) weights ordered accoarding to the energies (ascending)
 
@@ -60,9 +57,10 @@ class MF_loop:
                 expectation values of particle numbers on every site, ordered in the usual way
         '''
 
-        return (2/Z)*(np.absolute(eivec) @ weights)
+        return (2/Z)*(np.absolute(self.eivec) @ weights)
     
-    def Chi_calc(s, e, weights, eivec):  # keep in mind: numpy is row first
+
+    def Chi_calc(self, s, e, weights):  # keep in mind: numpy is row first
         
         '''
         Calculates the expectation value of Chi on the link s s -> e.
@@ -77,8 +75,7 @@ class MF_loop:
                 counting convention: start with 1
         weights: array of floats
                 thermal (Boltzman) weights ordered according to the energies (ascending)
-        eivec: 2D array of floats
-                matrix of eigenvectors of Hamiltonian (eigenvectors are columns)
+
 
         Returns:
         -------
@@ -87,21 +84,44 @@ class MF_loop:
                 
         
         '''
+     
+        return weights @ (self.eivec[s-1][:]*np.conjugate(self.eivec[e-1][:]))
 
-        
-        return weights @ (eivec[s-1][:]*np.conjugate(eivec[e-1][:]))
 
+    def Chi_comp(self,s,e,Chi_new):
+
+	a = pop_link_dict[str(s) + str(e)][2]
+	b = pop_link_dict[str(s) + str(e)][3]
+
+	if dtype(a) == np.complex128:
+		Chi_old = a
+	
+	elif dtype(b) == np.complex128:
+		Chi_old = b
+
+	else: 
+		print('error: neither Chi is not  complex')
+	
+	if np.absolute(np.absolute(Chi_old) -np.absolute(Chi_new))  < self.Chi_abs_dif_bd and np.absulte(np.phase(Chi_old) - np.phase(Chi_new)) < Chi_ph_dif_bd:
+		return Chi_old, True
+
+	else: 
+		
+	 	
 
     def mu_comp(self):
 
        
-        if all(np.absolute(N_arr - 1) < self.N_diff_bound) == True:
+        if all(np.absolute(N_arr - 1) < self.N_dif_bound) == True:
 
             return mu_arr, True
 
         else: 
 
-            return mu_arr + self.mu_step*(N_arr - 1), False 
+            return mu_arr + 0.1*mu_step*np.random.rand*(N_arr - 1), False # Ill probably have to adjust the factor 0.1 later to fit the local energy scale	
+
+
+ 
         
 
 
