0e234a0a2a60cc81aee0f1cd14fc23440b723aa9
Eric Kürschner <erickuerschner@gmx.de>
2026-09-22 16:34:17 +0200

    eod 220926

--- kagome_lattice/angles.py
+++ kagome_lattice/angles.py
@@ -1,28 +1,28 @@
 import numpy as np
 
-phi = np.arccos(-(1+np.sqrt(3))/4)
-kappa = np.arccos(-(1-np.sqrt(3))/np.sqrt(12-2*np.sqrt(3)))
-theta = np.arccos(-(1-np.sqrt(3))/np.sqrt(12-2*np.sqrt(3)))
+phi = -np.pi/2
+kappa = -np.pi/2
+theta = 5*np.pi/12
 # kappa = np.arccos((1-np.sqrt(3))/(1+np.sqrt(3))*(np.cos(phi)/np.sin(phi))
 # theta = np.arccos((1-np.sqrt(3))/(4*np.sin(phi)))
 
-sigmazpx = ((1-np.sqrt(3))/2)*np.cos(phi)-((1+np.sqrt(3))/2)*np.sin(phi)*np.cos(kappa)
-sigmazpy = -((1+np.sqrt(3))/2)*np.cos(phi)-((1-np.sqrt(3))/2)*np.sin(phi)*np.cos(kappa)
+sigmazpx = ((1-np.sqrt(3))*np.cos(phi)-(1+np.sqrt(3))*np.sin(phi)*np.cos(kappa))/np.sqrt(8)
+sigmazpy = (-(1+np.sqrt(3))*np.cos(phi)-(1-np.sqrt(3))*np.sin(phi)*np.cos(kappa))/np.sqrt(8)
 
-sigmaxpx = ((1-np.sqrt(3))/2)*np.sin(phi)*np.cos(theta)-((1+np.sqrt(3))/2)*(np.sin(theta)*np.sin(kappa) - np.cos(phi)*np.cos(theta)*np.cos(kappa))
-sigmaxpy = -((1+np.sqrt(3))/2)*np.sin(phi)*np.cos(theta)-((1-np.sqrt(3))/2)*(np.sin(theta)*np.sin(kappa) - np.cos(phi)*np.cos(theta)*np.cos(kappa))
+sigmaxpx = ((1-np.sqrt(3))*np.sin(phi)*np.cos(theta)-(1+np.sqrt(3))*(np.sin(kappa)*np.sin(theta) - np.cos(phi)*np.cos(kappa)*np.cos(theta)))/np.sqrt(8)
+sigmaxpy = (-(1+np.sqrt(3))*np.sin(phi)*np.cos(theta)-(1-np.sqrt(3))*(np.sin(kappa)*np.sin(theta) - np.cos(phi)*np.cos(kappa)*np.cos(theta)))/np.sqrt(8)
 
-sigmaypx = -((1-np.sqrt(3))/2)*np.sin(phi)*np.sin(theta) - ((1+np.sqrt(3))/2)*(np.cos(theta)*np.sin(kappa)+np.cos(phi)*np.sin(theta)*np.cos(kappa))
-sigmaypy = ((1+np.sqrt(3))/2)*np.sin(phi)*np.sin(theta)-((1-np.sqrt(3))/2)*(np.cos(theta)*np.sin(kappa)+np.cos(phi)*np.sin(theta)*np.sin(kappa))
+sigmaypx = (-(1-np.sqrt(3))*np.sin(phi)*np.sin(theta) - (1+np.sqrt(3))*(np.sin(kappa)*np.cos(theta)+np.cos(phi)*np.cos(kappa)*np.sin(theta)))/np.sqrt(8)
+sigmaypy = ((1+np.sqrt(3))*np.sin(phi)*np.sin(theta)-(1-np.sqrt(3))*(np.sin(kappa)*np.cos(theta)+np.cos(phi)*np.cos(kappa)*np.sin(theta)))/np.sqrt(8)
 
 
-#print(sigmazpx)
-#print(sigmazpy)
-#print(sigmaxpx)
-#print(sigmaxpy)
-#print(sigmaypx)
-#print(sigmaypy)
+print(sigmazpx)
+print(sigmazpy)
+print(sigmaxpx)
+print(sigmaxpy)
+print(sigmaypx)
+print(sigmaypy)
 
-print(phi)
-print(kappa)
-print(theta)
+# print(phi)
+# print(kappa)
+# print(theta)
